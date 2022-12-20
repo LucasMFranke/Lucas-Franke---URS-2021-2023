5d14c7f41aec0d5812ad456703d02410913d887a
LucasMFranke <32496654+LucasMFranke@users.noreply.github.com>
2022-12-20 11:31:26 -0600

    Add files via upload
    Changed scripts to run on many different budget values

--- match_checker.jl
+++ match_checker.jl
@@ -1,33 +1,35 @@
 using CSV, Clustering, DataFrames, DelimitedFiles, Plots, Tables, Statistics
 
-unclustered_selected = CSV.read("C:\\Users\\lucas\\Documents\\ArcGIS\\Projects\\LFranke_WISPO_Proj\\Risk_DataTables\\Clustering Stats\\Cluster365\\Upgrades365.csv", DataFrame);
-
-checker_arr = []
-k_values = []
-for i in 1:365
-    push!(k_values, i)
-    current_selections = CSV.read("C:\\Users\\lucas\\Documents\\ArcGIS\\Projects\\LFranke_WISPO_Proj\\Risk_DataTables\\Clustering Stats\\Cluster"*string(i)*"\\Upgrades"*string(i)*".csv", DataFrame);
-    number_of_matches = 0
-    for j in 1:length(current_selections[!, 1])
-        try
-            if (current_selections[j, 1] in unclustered_selected[!, 1])
-                number_of_matches+=1
+match_df = DataFrame()
+for p in 1:20
+    checker_arr = []
+    k_values = []
+    budget = 150000000*p
+    local unclustered_selected = CSV.read("C:\\Users\\lucas\\Documents\\ArcGIS\\Projects\\LFranke_WISPO_Proj\\Risk_DataTables\\Clustering Stats\\Cluster365\\Upgrades365Budget"*string(budget)*".csv", DataFrame);
+    for i in 1:365
+        push!(k_values, i)
+        current_selections = CSV.read("C:\\Users\\lucas\\Documents\\ArcGIS\\Projects\\LFranke_WISPO_Proj\\Risk_DataTables\\Clustering Stats\\Cluster" * string(i) * "\\Upgrades"*string(i)*"Budget"*string(budget)*".csv", DataFrame);
+        number_of_matches = 0
+        for j in 1:length(current_selections[!, 1])
+            try
+                if (current_selections[j, 1] in unclustered_selected[!, 1])
+                    number_of_matches+=1
+                end
+            catch
+                continue
             end
-        catch
-            continue
         end
+        push!(checker_arr, number_of_matches)
     end
-    push!(checker_arr, number_of_matches)
+
+    colname = "Budget_$budget"
+    match_df[!, colname] = checker_arr
+    scatter(k_values, checker_arr)
+    savefig("C:\\Users\\lucas\\Documents\\ArcGIS\\Projects\\LFranke_WISPO_Proj\\Risk_DataTables\\Clustering Stats\\match_graph_budget_"*string(p)*".png")
 end
-print(checker_arr)
 
-match_numbers = DataFrame()
-match_numbers.Matches = checker_arr;
 mkpath("C:\\Users\\lucas\\Documents\\ArcGIS\\Projects\\LFranke_WISPO_Proj\\Risk_DataTables\\Clustering Stats");
-CSV.write("C:\\Users\\lucas\\Documents\\ArcGIS\\Projects\\LFranke_WISPO_Proj\\Risk_DataTables\\Clustering Stats\\match_numbers.csv", match_numbers);
-    
-
-scatter(k_values, checker_arr)
+CSV.write("C:\\Users\\lucas\\Documents\\ArcGIS\\Projects\\LFranke_WISPO_Proj\\Risk_DataTables\\Clustering Stats\\match_numbers_overall.csv", match_df);
 
 # data_with_extra_columns = CSV.read("C:\\Users\\lucas\\Documents\\ArcGIS\\Projects\\LFranke_WISPO_Proj\\Risk_DataTables\\20210101_20211231.csv", DataFrame);
 # check_365 = CSV.read("C:\\Users\\lucas\\Documents\\ArcGIS\\Projects\\LFranke_WISPO_Proj\\Risk_DataTables\\Clustering Stats\\Cluster365\\centers365.csv", DataFrame);
